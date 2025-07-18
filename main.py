ed39731774c32f9f4e02dee0dc1ec866479196b7
ikegamiryusuke <ryu.611.mr@gmail.com>
2025-07-18 16:04:21 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
+import requests
+
 def run():
-    print("Asoboon Agent 起動準備OK！")
+    url = "https://script.google.com/macros/s/AKfycbzW7T8iKLXHOd69OrODPbnxGke9DoTKtTcqioLHhCwZOvNVAOoNa2K3mfcMk5lqju7CVw/exec"
+    params = {"name": "池上さま"}
+
+    response = requests.get(url, params=params)
+    print("GASからの返答:")
+    print(response.text)
