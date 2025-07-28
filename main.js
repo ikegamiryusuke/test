ecc60b66c5e61488d1d107252b0b258b0c9e4432
ikegamiryusuke <ryu.611.mr@gmail.com>
2025-07-28 12:07:45 +0900

    Remove PC warning and optimize layout

--- main.js
+++ main.js
@@ -402,12 +402,4 @@ function setup() {
   restoreFromLocal();
 }
 
-function checkMobile() {
-  if (window.innerWidth > 600 && !/Mobi/i.test(navigator.userAgent)) {
-    document.getElementById('pc-warning').style.display = 'flex';
-  } else {
-    setup();
-  }
-}
-
-document.addEventListener('DOMContentLoaded', checkMobile);
+document.addEventListener('DOMContentLoaded', setup);
